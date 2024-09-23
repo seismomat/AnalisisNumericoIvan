b868f2e5f62a890860b49a005c1a3e735066ee4f
seismomat <33993178+seismomat@users.noreply.github.com>
2024-09-23 17:40:36 -0600

    Update Sustituciones.py

--- Sustituciones.py
+++ Sustituciones.py
@@ -10,15 +10,18 @@ Created on Wed Sep 11 14:51:04 2024
 # matriz L, vector b ya estan dados como parametros
 # guardar los resultados en el vector y
 # Ly=b
-def sustDelante(L, b):
-    n=len(L)
-    y=np.empty_like(b)
-    y[0] = b[0]
-    for i in range(1,n):
-        y[i] = b[i]
-        for j in range(0,i):
-            y[i] -= L[i][j]*y[j]
-    return y
+
+def SustDelante(L,b):
+  x=np.zeros_like(b)
+  n=L.shape[0]# cantidad de renglones de L
+  #x[0]=b[0]/L[0,0]
+  for i in range(n):
+    sum=0.0
+    for j in range(i):
+      sum+=L[i,j]*x[j]
+    x[i]=(b[i]-sum)/L[i,i]
+
+  return x
 
 # algoritmo para sustitucion hacia atras
 # n es el tamano de la dimension del problema
@@ -39,24 +42,24 @@ def sustAtras(U, y):
 import numpy as np
 from numpy import linalg as LA
 
-U = np.array([[-4,-3,1],[0,5,1],[0,0,3]])
+U = np.array([[1.0,0.,0.],[-2.,1.,0.],[-1.,3.,1.]])
 L = np.array([[1,0,0],[-2,1,0],[-1,3,1]])
 
 A=L@U
 
 # Se definene los vectores
-b = np.array([5.0,6.0,1.0])
+b = np.array([1.0,1.0,1.0])
 # Usamos el algoritmo para encontrar la solucion
-y = sustDelante(L, b)
+y = SustDelante(L, b)
 
 # Se imprime el resultado
 print(y)
 
-# Usamos el algoritmo para encontrar la solucion
-x = sustAtras(U, y)
+# # Usamos el algoritmo para encontrar la solucion
+# x = sustAtras(U, y)
 
-# Se imprime el resultado
-print(x)
+# # Se imprime el resultado
+# print(x)
 
-print("comprobacion de los resultados")
-print(LA.solve(A,b))
+# print("comprobacion de los resultados")
+# print(LA.solve(A,b))
